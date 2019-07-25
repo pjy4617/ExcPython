a187d9d78abcb99b07298566772231fa3e041efa
jyPark <pjy4617@naver.com>
2019-07-25 16:37:45 +0900

    처음

---
+++ PythonApplication1/PythonApplication1.py
@@ -0,0 +1,9 @@
+
+import tkinter
+window = tkinter.Tk()
+window.title("test")
+window.geometry("640x400+100+100")
+window.resizable(False, False)
+label=tkinter.Label(window, text="Hello")
+label.pack();
+window.mainloop()

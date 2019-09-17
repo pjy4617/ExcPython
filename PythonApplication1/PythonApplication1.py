b2860835f96dcb884766b48d80d477a84d7b7ffc
jyPark <pjy4617@naver.com>
2019-09-17 09:53:42 +0900

    테스트

--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -1,7 +1,7 @@
 
 import tkinter
 window = tkinter.Tk()
-window.title("test")
+window.title("test4321")
 window.geometry("640x400+100+100")
 window.resizable(False, False)
 label=tkinter.Label(window, text="Hello")
